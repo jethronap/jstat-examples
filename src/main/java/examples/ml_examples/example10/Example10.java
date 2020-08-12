345c402a9949c8119349684ae4d112b829044cc3
pockerman <a.giavaras@gmail.com>
2020-08-12 11:47:50 +0100

    Update example 10

--- src/main/java/examples/ml_examples/example10/Example10.java
+++ src/main/java/examples/ml_examples/example10/Example10.java
@@ -31,19 +31,20 @@ public class Example10 {
         // create the emission probability matrix
         INDArray B = Nd4j.zeros(3, 3);
 
-        B.putScalar(0,0, 0.5);
-        B.putScalar(0,1, 0.25);
-        B.putScalar(0,2, 0.25);
-        B.putScalar(1,0, 0.1);
-        B.putScalar(1,1, 0.8);
-        B.putScalar(1,2, 0.1);
-        B.putScalar(2,0, 0.3);
-        B.putScalar(2,1, 0.15);
-        B.putScalar(2,2, 0.6);
+        B.putScalar(0,0, 0.16);
+        B.putScalar(0,1, 0.26);
+        B.putScalar(0,2, 0.58);
+        B.putScalar(1,0, 0.25);
+        B.putScalar(1,1, 0.28);
+        B.putScalar(1,2, 0.47);
+        B.putScalar(2,0, 0.2);
+        B.putScalar(2,1, 0.1);
+        B.putScalar(2,2, 0.7);
 
         // create the initialization vector
         INDArray pi = Nd4j.create(new double[]{0.7, 0.15, 0.15});
 
+        // create a sequence of observations
         List<String> sequence = new ArrayList<String>();
         sequence.add("a");
         sequence.add("b");
@@ -51,14 +52,25 @@ public class Example10 {
         sequence.add("c");
         sequence.add("b");
         sequence.add("a");
+
+        // map the sequnce observation to a column index
         Map<String, Integer> obsToIdx = new HashMap<>();
         obsToIdx.put("a", 0);
         obsToIdx.put("b", 1);
         obsToIdx.put("c", 2);
 
+        // compute alpha
         INDArray alpha = HMMHelpers.forward(sequence, A, B, pi, obsToIdx);
 
+        System.out.println("alpha matrix: ");
         System.out.println(alpha);
 
+        // we can now calculate the probability
+        double p = 0.0;
+        for(int i=0; i<A.shape()[0]; ++i){
+            p += alpha.getDouble(sequence.size()-1, i);
+        }
+
+        System.out.println("probability: " + p);
     }
 }
