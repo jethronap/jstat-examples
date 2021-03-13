ed760af186ac603f240bacb541cffaf24e86615e
Alex <a.giavaras@gmail.com>
2021-03-13 20:12:36 +0000

    Fix example

--- src/main/java/examples/ml_examples/example2/Example2.java
+++ src/main/java/examples/ml_examples/example2/Example2.java
@@ -1,67 +1,62 @@
 package examples.ml_examples.example2;
 
-import datasets.VectorDouble;
-import optimization.GradientDescent;
-import optimization.GDInput;
-import utils.DefaultIterativeAlgorithmController;
-import utils.IterativeAlgorithmResult;
-import datasets.DenseMatrixSet;
-import datastructs.RowBuilder;
-import datastructs.RowType;
-import maths.errorfunctions.MSEVectorFunction;
-import maths.functions.NonLinearVectorPolynomial;
-import maths.functions.ScalarMonomial;
-import ml.regression.NonLinearRegressor;
-import tech.tablesaw.api.Table;
-import utils.TableDataSetLoader;
 
-import java.io.File;
+import jstat.base.Configuration;
+import jstat.dataloader.CSVDataLoader;
+import jstat.maths.errorfunctions.MSEFunction;
+import jstat.ml.trainers.SupervisedTrainer;
+import jstat.utils.Pair;
+import jstat.ml.trainers.SupervisedTrainer;
+import jstat.optimization.GradientDescent;
+import jstat.optimization.GDInput;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import jstat.maths.errorfunctions.MSEFunction;
+import jstat.ml.regression.NonLinearRegressor;
+import jstat.maths.functions.NonLinearVectorPolynomial;
+import jstat.maths.functions.ScalarMonomial;
+
+
 import java.io.IOException;
 
-/** Category: Machine Learning
- * ID: Example7
- * Description: Non-linear Regression
- * Taken From:
- * Details:
- * TODO
- */
 
 public class Example2 {
 
         public static void main(String[] args)throws IOException {
 
-            // load the data
-            Table dataSet = TableDataSetLoader.loadDataSet(new File("src/main/resources/datasets/car_plant.csv"));
+            // set the data directory
+            Configuration.dataDirectory = "/home/alex/qi3/jstat/src/main/resources/jstat/datasets/";
 
-            VectorDouble labels = new VectorDouble(dataSet, "Electricity Usage");
-            Table reducedDataSet = dataSet.removeColumns("Electricity Usage").first(dataSet.rowCount());
-
-            DenseMatrixSet denseMatrixSet = new DenseMatrixSet(RowType.Type.DOUBLE_VECTOR, new RowBuilder(), reducedDataSet.rowCount(), 2, 1.0);
-            denseMatrixSet.setColumn(1, reducedDataSet.doubleColumn(0));
-            denseMatrixSet.duplicateColumn(1);
+            // load data set
+            Pair<INDArray, INDArray> dataSet = CSVDataLoader.loadCarPlantWithIntercept(1);
 
             // assume a hypothesis of the form w0 +w1*X + w2*X^2
-            // initially all weights are set o zeor
+            // initially all weights are set o zero
             NonLinearVectorPolynomial hypothesis = new NonLinearVectorPolynomial(new ScalarMonomial(0, 0.0),
-                    new ScalarMonomial(1, 0.0),
-                    new ScalarMonomial(2, 0.0));
+                                                                                 new ScalarMonomial(1, 0.0),
+                                                                                 new ScalarMonomial(2, 0.0));
 
             // the regressor
-            NonLinearRegressor regressor = new NonLinearRegressor(hypothesis);
+            NonLinearRegressor regression = new NonLinearRegressor(hypothesis);
 
+            // since we do linear regression we will use
+            // mean square error as the loss function
+            MSEFunction mse = new MSEFunction(regression);
 
+            // configuration parameters for optimizer
             GDInput gdInput = new GDInput();
             gdInput.showIterations = true;
             gdInput.eta = 0.001;
-            gdInput.errF = new MSEVectorFunction(hypothesis);
-            gdInput.iterationContorller = new DefaultIterativeAlgorithmController(10000, 1.0e-8);
+            gdInput.lossFunction = mse;
+            gdInput.parameters = regression.getParameters();
 
+            // the optimizer
             GradientDescent gdSolver = new GradientDescent(gdInput);
 
-            IterativeAlgorithmResult result = (IterativeAlgorithmResult) regressor.train(denseMatrixSet, labels, gdSolver);
+            // The object responsible for the training
+            SupervisedTrainer trainer = new SupervisedTrainer(regression, gdSolver, mse, 50, 1.0e-5);
+            trainer.train(dataSet.first, dataSet.second);
 
-            System.out.println(result);
-            System.out.println("Intercept: " + hypothesis.getCoeff(0) + " slope 1: " + hypothesis.getCoeff(1) + " slope 2" + hypothesis.getCoeff(2));
+            System.out.println("GD Optimization Coefficients "+ regression.getCoeffs());
 
         }
 }
