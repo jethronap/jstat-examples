9b28285ffde5409ec88c74cd1b781313ac3e6a2b
Alex <a.giavaras@gmail.com>
2021-03-21 18:03:07 +0000

    Fix examples removed notebooks and html files

--- src/main/java/examples/ml_examples/example4/Example4.java
+++ src/main/java/examples/ml_examples/example4/Example4.java
@@ -1,26 +1,21 @@
 package examples.ml_examples.example4;
 
-import datasets.DenseMatrixSet;
-import datasets.VectorDouble;
-import datastructs.RowBuilder;
-import datastructs.RowType;
-import maths.functions.distances.EuclideanVectorCalculator;
-import ml.classifiers.ThreadedKNNClassifier;
-import parallel.partitioners.MatrixRowPartitionPolicy;
-import parallel.partitioners.RangePartitioner;
-import tech.tablesaw.api.Table;
-import tech.tablesaw.columns.Column;
-import ml.classifiers.utils.ClassificationVoter;
-import utils.Pair;
-import utils.PairBuilder;
-import utils.TableDataSetLoader;
 
-import java.io.File;
+
+import jstat.base.Configuration;
+import jstat.dataloader.CSVDataLoader;
+import jstat.maths.functions.distances.EuclideanMetric;
+import jstat.ml.classifiers.ThreadedKNNClassifier;
+import jstat.ml.classifiers.utils.ClassificationVoter;
+import jstat.utils.Pair;
+import jstat.utils.PairBuilder;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
-
 import static java.util.concurrent.Executors.newFixedThreadPool;
 
 
@@ -33,68 +28,34 @@ import static java.util.concurrent.Executors.newFixedThreadPool;
  */
 public class Example4 {
 
-    public static Pair<DenseMatrixSet<Double>, List<Integer>> createDataSet() throws IOException, IllegalArgumentException {
-
-        // load the data
-        Table dataSetTable = TableDataSetLoader.loadDataSet(new File("src/main/resources/datasets/iris_data.csv"));
-
-        List<Integer> labels = new ArrayList<>();
-
-        Column species  = dataSetTable.column("species");
-
-        for (int i = 0; i < species.size(); i++) {
-
-            String label = (String) species.get(i);
-
-            if(label.equals("Iris-setosa")){
-
-                labels.add(0);
-            }
-            else if(label.equals("Iris-versicolor")){
-
-                labels.add(1);
-            }
-            else if(label.equals("Iris-virginica")){
-
-                labels.add(2);
-            }
-            else{
-                throw new IllegalArgumentException("Unknown class");
-            }
-        }
-
-        Table reducedDataSet = dataSetTable.removeColumns("species").first(dataSetTable.rowCount());
-        DenseMatrixSet<Double> dataSet = new DenseMatrixSet(RowType.Type.DOUBLE_VECTOR, new RowBuilder());
-        dataSet.initializeFrom(reducedDataSet);
-
-        // partition the data set
-        List<List<Integer>> partitions = RangePartitioner.partition(0, dataSet.m(), 4);
-
-        MatrixRowPartitionPolicy partitionPolicy = new MatrixRowPartitionPolicy(partitions);
-        dataSet.setPartitionPolicy(partitionPolicy);
-
-        return PairBuilder.makePair(dataSet, labels);
-    }
-
     public static void main(String[] args) throws IOException, IllegalArgumentException{
 
-        Pair<DenseMatrixSet<Double>, List<Integer>> data = Example4.createDataSet();
+        // set the data directory
+        Configuration.dataDirectory = "/home/alex/qi3/jstat/src/main/resources/jstat/datasets/";
+
+        // load data set
+        Pair<INDArray, INDArray> dataSet = CSVDataLoader.loadIrisData();
+
         ExecutorService executorService = newFixedThreadPool(4);
 
-        System.out.println("Number of rows: "+data.first.m());
-        System.out.println("Number of labels: "+data.second.size());
+        System.out.println("Number of rows: "+dataSet.first.size(0));
+        System.out.println("Number of labels: "+dataSet.second.size(0));
 
+        List<Pair<Integer, Integer>> partitions = new ArrayList<>(4);
+        partitions.add(PairBuilder.makePair(0, 37));
+        partitions.add(PairBuilder.makePair(37, 2*37));
+        partitions.add(PairBuilder.makePair(2*37, 3*37));
+        partitions.add(PairBuilder.makePair(3*37, (int) dataSet.first.size(0)));
 
-        ThreadedKNNClassifier<Double, DenseMatrixSet<Double>, EuclideanVectorCalculator<Double>,
-                ClassificationVoter> classifier = new ThreadedKNNClassifier<>(3, false, executorService);
+        ThreadedKNNClassifier classifier = new ThreadedKNNClassifier(3, false, partitions, executorService);
 
-        classifier.setDistanceCalculator(new EuclideanVectorCalculator<Double>());
+        classifier.setDistanceCalculator(new EuclideanMetric());
         classifier.setMajorityVoter(new ClassificationVoter());
 
-        classifier.train(data.first, data.second);
-        VectorDouble point = new VectorDouble(5.9,3.0,5.1,1.8);
-        Integer classIdx = classifier.predict(point);
+        classifier.train(dataSet.first, dataSet.second);
+        INDArray point = Nd4j.create(new double[]{5.9,3.0,5.1,1.8});
 
+        Integer classIdx = classifier.predictPoint(point);
         System.out.println("Point "+ point +" has class index "+ classIdx);
         executorService.shutdown();
 
